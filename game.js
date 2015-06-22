af4e81e37170fb051e0992e3baa5a1462a0bbb9c
Jimmy Li <jimmy.li@bluelabs.com>
2015-06-22 15:34:57 -0700

    set pieces

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var Board = function (rows, cols, wrapperId) {
 	this.board = [];
 };
 
-Board.prototype.draw = function () {
+Board.prototype.drawBlank = function () {
 	var tableHTML = "<table id=" + 'game-board' + ">"
 	var countForColor = 0
 	for (var i = 0; i < this.rows; i++) {
@@ -26,10 +26,11 @@ Board.prototype.draw = function () {
 	document.getElementById(this.wrapperId).innerHTML = tableHTML;	
 }
 
-Board.prototype.setPieces = function (topPlayer, bottomPlayer) {
+Board.prototype.setVirtualPieces = function (topPlayer, bottomPlayer) {
 	var countForPlacement = 0;
 	for (var i = 0; i < this.rows; i++) {
 		this.board.push([]);
+		countForPlacement += 1;
 		for (var j = 0; j < this.rows; j++) {
 			if (i < 3 && countForPlacement % 2 == 0) {
 				this.board[this.board.length - 1].push(new Piece(topPlayer, i, j));
@@ -41,8 +42,20 @@ Board.prototype.setPieces = function (topPlayer, bottomPlayer) {
 			countForPlacement += 1;
 		}
 	}
-	debugger;
 }
+
+Board.prototype.drawPieces = function (topPlayer, bottomPlayer) {
+	for (var i = 0; i < this.rows; i++) {
+		for (var j = 0; j < this.rows; j++) {
+			if (this.board[i][j].owner === topPlayer) {
+				document.getElementById(i + '-' + j).innerHTML = 'O';
+			} else if (this.board[i][j].owner === bottomPlayer) {
+				document.getElementById(i + '-' + j).innerHTML = 'X';
+			}
+		}
+	}
+}
+
 
 var Piece = function (owner, yPos, xPos) {
 	this.owner = owner;
@@ -58,6 +71,7 @@ window.onload = function () {
 	var board = new Board(10, 10, 'board-wrapper')
 	var topPlayer = new Player();
 	var bottomPlayer = new Player();
-	board.draw();
-	board.setPieces();
+	board.drawBlank();
+	board.setVirtualPieces(topPlayer, bottomPlayer);
+	board.drawPieces(topPlayer, bottomPlayer);
 }
